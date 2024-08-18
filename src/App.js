5ebd8170bd4217e58307e7617ff6de172a08e75d
Priyanka yadav <priyankayadavyy01@gmail.com>
2024-08-18 17:00:55 +0530

    curd changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddCategory from "./component/AddCategory";
+import Category from "./component/Category";
+import MainNav from "./component/MainNav";
+import Footer from "./component/Footer";
+import NotFound from "./component/NotFound";
+import Cantact from "./component/Cantact";
+import LogOut from "./component/LogOut";
+import Detail from "./component/Detail";
+import Update from "./component/Update";
+import SignUp from "./component/SignUp";
+import LogIn from "./component/LogIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainNav />
+      <Routes>
+        <Route path="/category" element={<Category />} />
+        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/contact" element={<Cantact />} />
+        <Route path="/logout" element={<LogOut />} />
+        <Route path="detail/:id" element={<Detail />} />
+        <Route path="edit/:id" element={<Update />} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
